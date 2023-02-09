350d34f16f208dc88552bab1a6fde102d154512f
illyge <ilia.vishnevsky@gmail.com>
2023-02-09 16:33:13 +0100

    retrained for the data from ChatGPT and more various answers from StackOverflow. Changed service to use XGBoost

--- service.py
+++ service.py
@@ -52,7 +52,8 @@ Classify an answer to a StackOverflow question as an output from GPT AI or not
 @svc.api(input=JSON(pydantic_model=CounterAIApp), output=JSON(), doc=api_doc_string)
 def classify(twitter_object):
     data = prepare_data(pd.DataFrame(twitter_object.dict(), index=[0]), train=False)
-    prediction = model_runner.predict.run(pd.DataFrame(data=data, index=[0]))
+    prediction = model_runner.predict_proba.run(pd.DataFrame(data=data, index=[0]))
     return {
-        'ai_generated': prediction == 1
+        'ai_generated': prediction[0][1] > 0.5,
+        'ai_probability': prediction[0][1]
     }
--- train.py
+++ train.py
@@ -1,40 +1,59 @@
 import bentoml
 import pandas as pd
-from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import f1_score
 from sklearn.model_selection import train_test_split
-from sklearn.pipeline import Pipeline
-from util.train_util import create_pipeline
+from xgboost.sklearn import XGBClassifier
 from util.preprocess_util import prepare_data
+from util.train_util import create_pipeline
 
-def pipeline():
-    pipe = create_pipeline()
+
+def regression_pipeline():
+    pipe = create_pipeline(LogisticRegression())
     pipe.set_params(**{
         'nb_col_trans__nb_pipe__c_vect__min_df': 6,
         'nb_col_trans__nb_pipe__c_vect__ngram_range': (1, 3),
-        'lr__C': 10.0, 'lr__solver': 'lbfgs'})
+        'clf__C': 10.0, 'clf__solver': 'lbfgs', 'clf__max_iter': 500})
     return pipe
 
 
+def forest_pipeline():
+    pipe = create_pipeline(RandomForestClassifier())
+    pipe.set_params(**{'clf__max_depth': 50, 'clf__max_features': 'sqrt', 'clf__min_samples_leaf': 1,
+                       'clf__min_samples_split': 10, 'clf__n_estimators': 300})
+    return pipe
+
+def xgb_pipeline():
+    pipe = create_pipeline(XGBClassifier())
+    pipe.set_params(**{'clf__learning_rate': 0.3, 'clf__max_depth': 5, 'clf__min_child_weight': 3, 'clf__n_estimators': 85})
+    return pipe
+
 def train():
-    data = pd.read_json("data/preprocessed.jsonl", lines=True)
+    data = pd.read_json("data/balanced_data.jsonl", lines=True)
+    data = prepare_data(data, train=False)
 
     df_full_train, df_test = train_test_split(data, test_size=0.2, random_state=30)
-    selected_features = ['answer_length', 'creativity', 'stealing_strength', 'sentence_length_mean', 'vocabulary_size']
+    selected_features = ['creativity', 'stealing_strength', 'stealing_frequency', 'sentence_length_mean',
+                         'sentence_length_std', 'answer_length', 'vocabulary_size']
 
     X_train = df_full_train[['stemmed_answer'] + selected_features]
     y_train = df_full_train.target
     X_test = df_test[['stemmed_answer'] + selected_features]
     y_test = df_test.target
 
-    classifier = pipeline()
+    classifier = forest_pipeline()
     classifier.fit(X_train, y_train)
 
     print(f"Score: {f1_score(classifier.predict(X_test), y_test)}")
 
     print(bentoml.sklearn.save_model(
         'counter-ai-model',
-        classifier
+        classifier,
+        signatures={
+            "predict": {"batchable": False},
+            "predict_proba": {"batchable": False},
+        },
     ))
 
 
